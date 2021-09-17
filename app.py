7cab4ba3c364b049e4e05bf6dde56402f2158d57
vincenttc-1 <vincentcengtjendra@gmail.com>
2021-09-17 21:36:08 +0700

    update

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ from flask_cors import CORS
 from flask import Flask,jsonify,request
 
 app = Flask(__name__)
-if name == 'main':
+if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
 @app.route("/")
